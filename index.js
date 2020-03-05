27c8e398b0fd20e7265bd9d9130021401a54a543
Chris Wong <wc38.wong@gmail.com>
2020-03-05 15:53:35 +0800

    Use ESLint

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+const log = console.log
+
+/* eslint-disable no-console */
+
+log(`Get started
+init command
+1. change hooks path
+2. make directory .githooks
+3. run .githooks/pre-commit scritp
+`)
